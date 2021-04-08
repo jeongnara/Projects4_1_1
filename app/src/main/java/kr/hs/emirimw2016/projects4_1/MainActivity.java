7c4aa8924df32a6b297171954546cd6142970eca
user <w2016@e-mirim.hs.kr>
2021-04-08 19:52:20 +0900

    Update Error Toast Massage

--- app/src/main/java/kr/hs/emirimw2016/projects4_1/MainActivity.java
+++ app/src/main/java/kr/hs/emirimw2016/projects4_1/MainActivity.java
@@ -3,11 +3,13 @@ package kr.hs.emirimw2016.projects4_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -39,6 +41,11 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View v) {
             String strEdit1 = edit1.getText().toString();
             String strEdit2 = edit2.getText().toString();
+            Log.v("***strEdid1", strEdit1);
+            if(strEdit1.equals("")|| strEdit2.equals("")){
+                Toast.makeText(getApplicationContext(),  "값을 입력하지 않았습니다.", Toast.LENGTH_SHORT).show();
+                return;
+            }
             int num1 = Integer.parseInt(strEdit1);
             int num2 = Integer.parseInt(strEdit2);
             int result = 0;
